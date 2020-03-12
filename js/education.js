dae1060968bf5afbaec614a6eb7f5794adc880e2
David Gilkeson <davidgilkeson8@gmail.com>
2020-03-12 01:47:12 +0000

    Initial commit

---
+++ js/education.js
@@ -0,0 +1,24 @@
+const counters = document.querySelectorAll('.numbers');
+const speed = 200;
+
+counters.forEach(numbers => {
+
+    const updateCount = () => {
+
+        const target = +numbers.getAttribute('data-target');
+        const count = +numbers.innerText;
+
+        const inc = target / speed;
+
+        if (count < target) {
+
+            numbers.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, 8);
+
+        } else {
+
+            count.innerText = target;
+        }
+    }
+    updateCount();
+});
