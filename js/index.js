5e96eb5feed22909215a62f0c38c1d646c052b32
David Gilkeson <davidgilkeson8@gmail.com>
2020-04-09 04:27:54 +0100

    Quote generator fetched and added

--- js/index.js
+++ js/index.js
@@ -16,35 +16,24 @@ navLinks.forEach(link => {
 
 // Quotes Slider
 
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
 
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout("showSlides()", 2000);
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 
 }
-
-//setTimeout("showSlides()", 2000);
-window.onload = showSlides;
---
+++ js/quote.js
@@ -0,0 +1,29 @@
+const newQuote = document.querySelector("#new-quote");
+const quote = document.querySelector("#text");
+const author = document.querySelector("#author");
+
+
+let quoteObject;
+
+//add quote when page loads
+window.onload = getQuote;
+
+// //change quote when button is clicked
+// setTimeout('getQuote()', 5000);
+
+//function to get and assign quote and author
+function getQuote() {
+    //fetch the data
+    fetch('https://quota.glitch.me/random')
+        .then(res => res.json()) //response type
+        .then(data => {
+            quoteObject = data; // assign data to quoteObject
+            //display quote
+            quote.textContent = quoteObject["quoteText"];
+            //display author
+            author.innerHTML = quoteObject['quoteAuthor'];
+
+        });
+    //change quote when button is clicked
+    setTimeout('getQuote()', 5000);
+}
